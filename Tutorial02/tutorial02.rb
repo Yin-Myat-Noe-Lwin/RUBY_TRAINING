4c1956f71d0ffc569cd1dbca6fb4f5f1c794e5dc
lwinleo05 <yinmyatnoelwin1@gmail.com>
2022-09-29 00:17:20 -0700

    Tutorial02

--- Tutorial02/tutorial02.rb
+++ Tutorial02/tutorial02.rb
@@ -7,12 +7,12 @@ cmonth=now.month
 
 cday=now.day
 
-print "Enter your date of birth(YYYY/MM/DD):"
+print "Enter your date of birth(YYYY-MM-DD):"
 
 input=gets.chomp
 #user input
 
-input_array=input.split '/'
+input_array=input.split ("-")
 #change input into array
 
 age= cyear - input_array[0].to_i- ((cmonth > input_array[1].to_i || (cmonth == input_array[1].to_i && cday >= input_array[2].to_i)) ? 0 : 1)
