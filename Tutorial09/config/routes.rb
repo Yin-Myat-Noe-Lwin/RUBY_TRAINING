90a1d6d582ff9a5332c6583c30e97d00eb0b78d0
lwinleo05 <yinmyatnoelwin1@gmail.com>
2022-10-06 15:39:59 -0700

    Tutorial09

--- Tutorial09/config/routes.rb
+++ Tutorial09/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get "/new", to: "images#new"
 
-  get "/success", to: "images#success"
+  post "/success", to: "images#success"
 
   get "/fail" ,  to: "images#fail"
   
