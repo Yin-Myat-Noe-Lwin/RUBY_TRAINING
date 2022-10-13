053eb19811185d687216b24675c1928b1291fc18
lwinleo05 <yinmyatnoelwin1@gmail.com>
2022-10-13 14:32:16 -0700

    Tutorial09 Update

--- Tutorial09/app/controllers/images_controller.rb
+++ Tutorial09/app/controllers/images_controller.rb
@@ -11,27 +11,40 @@ class ImagesController < ApplicationController
     @image =  params[:image]
 
     @folder =  params[:folder]
-    
-    begin
 
-      #Create directory with user input
-      Dir.mkdir( 'app/assets/images/' + @folder.to_s )
+    @extension = File.extname(@image)
 
-      File.open( Rails.root.join('app/assets/images/' + @folder.to_s , @image.original_filename ), 'wb') do |file|
-        
-      #Put image into created directory
-      file.write( @image.read )
+    if @extension.to_s == ".jpg" || @extension.to_s == ".png"
+
+      begin
+
+        #Create directory with user input
+        Dir.mkdir( 'app/assets/images/' + @folder.to_s )
+
+        File.open( Rails.root.join('app/assets/images/' + @folder.to_s , @image.original_filename ), 'wb') do |file|
+          
+        #Put image into created directory
+        file.write( @image.read )
+
+        end
+
+      rescue Exception=>e
+
+        #show flash message if folder name already exists
+        flash.alert = "Failed to save image. Folder name already exists."
+
+        redirect_to fail_url
 
       end
 
-    rescue Exception=>e
+  else
 
-      #show flash message if folder name already exist
-      flash.alert = "Failed to save image. Folder name already exists."
+    #show flash message if input file is not image
+    flash.alert = "Unsupported File Type"
 
       redirect_to fail_url
 
-    end
+  end
 
   end
 
