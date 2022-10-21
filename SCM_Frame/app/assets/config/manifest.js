738fafb880231a6ba7879afd3f04cad456649500
lwinleo05 <yinmyatnoelwin1@gmail.com>
2022-10-21 09:51:33 -0700

    SCM Frame

--- SCM_Frame/app/assets/config/manifest.js
+++ SCM_Frame/app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .js
+//= link_tree ../../../vendor/javascript .js
+//= link controllers/application.js
+ //= link controllers/hello_controller.js
+ //= link controllers/index.js
